dff13da2cc9edde676cbc18f0149830a74cee19a
Dmitriy Fedorov <dmitriy.fedoroff@gmail.com>
2019-01-21 14:45:44 +0300

    commit

---
+++ lesson_8/maximum.py
@@ -0,0 +1,16 @@
+# maximum.py
+
+def maximum(*numbers):
+    """
+    >>> maximum(3, 2, 8)
+    8
+    >>> maximum(1, 5, 9, -2, 2)
+    9
+    >>> maximum()    
+    """
+    # здесь ваш код
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    
