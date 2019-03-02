6805f133ef07c04faecc0c8973e4e52c6dfe4979
Dmitriy Fedorov <dmitriy.fedoroff@gmail.com>
2019-03-02 19:16:13 +0300

    fix

--- lesson_10/sberbank.py
+++ lesson_10/sberbank.py
@@ -11,8 +11,8 @@ def money_reader(what, where, when):
     import csv
     money = list()
     try:
-        with open('opendata.csv', encoding='cp1251') as csvfile:
-            money_reader = csv.reader(csvfile, delimiter=',') # delimiter по умолчанию ','
+        with open('opendata.csv') as csvfile:
+            money_reader = csv.reader(csvfile) # delimiter по умолчанию ','
             for row in money_reader:
                 if row[0] == what:
                     if row[1] == where:                    
@@ -24,9 +24,11 @@ def money_reader(what, where, when):
         return e
 
 if __name__ == '__main__':
+
     print('Санкт-Петербург')
     for year in range(2015, 2019):
         print(str(year), money_reader('Средняя зарплата', 'Санкт-Петербург', str(year)))
+
     print('Москва')
     for year in range(2015, 2019):
         print(str(year), money_reader('Средняя зарплата', 'Москва', str(year)))        
