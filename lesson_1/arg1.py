221850127d224e23878fbf8ff24357229bbc69fb
Dmitriy Fedorov <dmitriy.fedoroff@gmail.com>
2019-09-06 08:42:53 +0300

    args fixx

--- lesson_1/arg1.py
+++ lesson_1/arg1.py
@@ -1,7 +1,7 @@
 # arg1.py
 # ключевые аргументы
 def func(a, b=5, c=10): 
-    print('a равно', a, ', b равно', b, ', а c равно', c) 
+    return a, b, c 
 
 print(func(3, 7)) 
 print(func(25, c=24)) 
