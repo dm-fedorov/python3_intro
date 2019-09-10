fa5db183553f25a70b3cc8d4e07b32af28590a66
Dmitriy Fedorov <dmitriy.fedoroff@gmail.com>
2019-09-10 11:13:12 +0300

    Update my_flatten.py

--- lesson_11/recursion/my_flatten.py
+++ lesson_11/recursion/my_flatten.py
@@ -3,9 +3,9 @@ my_arr = [1, 2, [3, [4, 5]], 6]
 def my_flatten(arr, res=[]):
     """
     >>> print(my_flatten([1, 2, [3, [4, 5]], 6]))
-    [1, 2, [3, [4, 5]], 6]
+    [1, 2, 3, 4, 5, 6]
     >>> print(my_flatten([1, 2, [3, [4, 5]], 6]))
-    [1, 2, [3, [4, 5]], 6]
+    [1, 2, 3, 4, 5, 6]
     """
     for item in arr:
         if type(item) == int:
